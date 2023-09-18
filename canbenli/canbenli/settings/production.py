95354d033eadfb533c8e041a1f8ee4defe9e7820
mustafa-bayhan <bayhanmu19@itu.edu.tr>
2023-09-18 20:01:13 +0000

    from server 20230918 2301

--- canbenli/canbenli/settings/production.py
+++ canbenli/canbenli/settings/production.py
@@ -2,7 +2,7 @@
 from canbenli.settings.base import *
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.canbenli.com','canbenli.com']
+ALLOWED_HOSTS = ['www.canbenli.com','canbenli.com','www.canbenli.com.tr','canbenli.com.tr']
 STATIC_ROOT =os.path.join(BASE_DIR,'static')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
